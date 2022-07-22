2c1f265fae3d835ab56d76035342a394e30d0f50
ShindanoSingi <shindanosingi@sekur.com>
2022-07-21 23:47:49 -0500

    bembechat deployed on heroku

--- bembechat_django/settings.py
+++ bembechat_django/settings.py
@@ -67,7 +67,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'channels',
     'bembechat',
-    'material',
+    # 'material',
     'fontawesomefree',
     'room',
     'widget_tweaks',
